64d9198d4ad21f9e63460b992825b4e78ae2d945
FedericaI7 <125682865+FedericaI7@users.noreply.github.com>
2023-11-01 12:44:34 +0100

    Exercise 1-11-23
    Edgemony course

---
+++ 1-11-23/script.js
@@ -0,0 +1,14 @@
+let arrAnimals = [
+  "leone",
+  "coniglio",
+  "tigre",
+  "serpente",
+  "gabbiano",
+  "squalo",
+  "pappagallo",
+  "ippopotamo",
+];
+
+for (let i = 0; i < arrAnimals.length; i++) {
+  console.log(i + 1 + ": " + arrAnimals[i]);
+}
